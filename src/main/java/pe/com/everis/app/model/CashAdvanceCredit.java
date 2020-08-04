3167764fbb89300eb8e1bc13f5bf567a8d9a3c44
alessandra238 <arosadov@everis.com>
2020-08-03 23:44:21 -0500

    credits

---
+++ src/main/java/pe/com/everis/app/model/CashAdvanceCredit.java
@@ -0,0 +1,8 @@
+package pe.com.everis.app.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "CashAdvanceCredit")
+public class CashAdvanceCredit {
+
+}
